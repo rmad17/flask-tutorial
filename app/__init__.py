d0bfefc418309fbfa9997b35ace8375af03ebb71
rmad17 <souravbasu17@gmail.com>
2015-12-06 16:39:10 +0530

    Completed templates

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2015 rmad <rmad@rmad-mint>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+from flask import Flask
+
+app = Flask(__name__)
+from app import views
--- run.py
+++ run.py
@@ -9,14 +9,6 @@
 """
 
 """
-
-from flask import Flask
-app = Flask(__name__)
-
-
-@app.route("/")
-def hello():
-    return "Hello World!"
-
-if __name__ == "__main__":
-    app.run()
+#!flask/bin/python
+from app import app
+app.run(debug=True)
